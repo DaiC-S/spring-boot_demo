90c66d90913125137012211e750b801399d67151
DaiC-S <yuangudazhi@gmail.com>
2024-03-20 22:11:41 +0900

    get and show  user info from DB

--- src/main/java/com/dvleo/springboot_demo/controller/DemoController.java
+++ src/main/java/com/dvleo/springboot_demo/controller/DemoController.java
@@ -1,13 +1,19 @@
 package com.dvleo.springboot_demo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
 
     @GetMapping("/")
-    public String index(){
+    public String viewHome(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+        model.addAttribute("email", email);
         return "index";
     }
 }
